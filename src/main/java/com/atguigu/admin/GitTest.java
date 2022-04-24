5b976063580e56d6f59273ea8a5e693c044458b1
3448576919 <76897230+3448576919@users.noreply.github.com>
2022-04-24 20:10:26 +0800

    pull test

--- src/main/java/com/atguigu/admin/GitTest.java
+++ src/main/java/com/atguigu/admin/GitTest.java
@@ -11,5 +11,6 @@ public class GitTest {
               System.out.println("fixfixfix");
               System.out.println("sssssssssss");
               System.out.println("111111111111");
+                System.out.println("22222222222222");
        }
 }
