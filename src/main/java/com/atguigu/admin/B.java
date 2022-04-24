1dcdbddac10aff8fce0feb38c90003c35751c237
3448576919 <3448576919@qq.com>
2022-04-24 18:54:52 +0800

    first commit

---
+++ src/main/java/com/atguigu/admin/B.java
@@ -0,0 +1,5 @@
+package com.atguigu.admin;
+
+
+public class B {
+}
---
+++ src/main/java/com/atguigu/admin/GitTest.java
@@ -0,0 +1,8 @@
+package com.atguigu.admin;
+
+
+public class GitTest {
+       public static void main(String[] args) {
+              System.out.println("tijiaochenggong");
+       }
+}
